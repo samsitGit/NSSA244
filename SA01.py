628f8ca62b49c13c8cf14f58a01123f8a2d8b9b3
Sam Sit <ss4521@rit.edu>
2023-03-14 16:18:51 -0400

    Task 2 WIP

--- SA01.py
+++ SA01.py
@@ -19,7 +19,32 @@ def text_menu():
     command = input(">> ")
     return command
 
+#Task 2
+def process_command(command):
+    try:
+        command = int(command)
+    except:
+        print("Invalid command. Please retry.\n")
+    if command == 1:
+        print("1")
+    elif command == 2:
+        print("2")
+    elif command == 3:
+        print("3")
+    elif command == 4:
+        print("4")
+    elif command == 5:
+        print("5")
+    elif command == 6:
+        print("6")
+    elif command == 7:
+        print("Command 7")
+        print("Exiting the program")
+        exit()
+    else:
+        print("Incorrect command. Please retry.\n")
+
 def main():
-    text_menu()
+    process_command(text_menu())
 
 main()
